7dc805b9bf64b8327c379bbff2d9cb09c2318245
nab138 <nab@nabdev.me>
2025-08-26 12:44:46 -0400

    Improve WSL path robustness

--- src-tauri/src/windows.rs
+++ src-tauri/src/windows.rs
@@ -1,19 +1,35 @@
 #[cfg(target_os = "windows")]
 use std::os::windows::process::CommandExt;
 use std::process::{Command, Stdio};
-#[cfg(target_os = "windows")]
-use wslpath2::{convert, Conversion};
 
 #[cfg(target_os = "windows")]
 pub fn windows_to_wsl_path(path: &str) -> Result<String, String> {
-    println!("Converting Windows path to WSL path: {}", path);
-    convert(path, None, Conversion::WindowsToWsl, false).map_err(|e| e.to_string())
+    let res = convert(path, Conversion::WindowsToWsl, false, true).map_err(|e| e.to_string());
+    match res {
+        Ok(path) => Ok(path),
+        Err(err) => {
+            let res2 = convert(path, Conversion::WindowsToWsl, false, false);
+            match res2 {
+                Ok(path) => Ok(path),
+                Err(_) => Err(err),
+            }
+        }
+    }
 }
 
 #[cfg(target_os = "windows")]
 pub fn wsl_to_windows_path(path: &str) -> Result<String, String> {
-    println!("Converting WSL path to Windows path: {}", path);
-    convert(path, None, Conversion::WslToWindows, false).map_err(|e| e.to_string())
+    let res = convert(path, Conversion::WslToWindows, false, true).map_err(|e| e.to_string());
+    match res {
+        Ok(path) => Ok(path),
+        Err(err) => {
+            let res2 = convert(path, Conversion::WslToWindows, false, false);
+            match res2 {
+                Ok(path) => Ok(path),
+                Err(_) => Err(err),
+            }
+        }
+    }
 }
 
 #[tauri::command]
@@ -40,4 +56,71 @@ pub fn has_wsl() -> bool {
 #[tauri::command]
 pub fn is_windows() -> bool {
     cfg!(target_os = "windows")
+}
+
+// Taken from wslpath2 crate and modified
+#[cfg(target_os = "windows")]
+#[derive(Debug)]
+pub enum Conversion {
+    /// Convert Windows path to WSL path
+    WindowsToWsl,
+    /// Convert WSL path to Windows path
+    WslToWindows,
+    /// Convert WSL path to Windows path using Linux style path separators
+    WslToWindowsLinuxStyle,
+}
+
+#[cfg(target_os = "windows")]
+pub fn convert(
+    path: &str,
+    options: Conversion,
+    force_absolute_path: bool,
+    replace_backslash: bool,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let mut args = Vec::new();
+
+    args.push("-e");
+    args.push("wslpath");
+
+    args.push(match options {
+        Conversion::WindowsToWsl => "-u",
+        Conversion::WslToWindows => "-w",
+        Conversion::WslToWindowsLinuxStyle => "-m",
+    });
+
+    if force_absolute_path {
+        args.push("-a");
+    }
+
+    let mut cmd = Command::new("wsl.exe");
+    cmd.args(args);
+    let real_path = if replace_backslash {
+        path.replace('\\', "\\\\")
+    } else {
+        path
+    };
+    cmd.arg(real_path);
+
+    #[cfg(windows)]
+    cmd.creation_flags(0x08000000); // CREATE_NO_WINDOW
+
+    println!("Executing command: {:?}", cmd);
+
+    let output = cmd
+        .output()
+        .map_err(|e| format!("Error executing wsl.exe: {}", e))?;
+
+    let code = output.status.code().unwrap_or(-1);
+    if code != 0 {
+        println!(
+            "wslpath output: {}",
+            String::from_utf8_lossy(&output.stdout)
+        );
+        return Err(format!("Error getting wslpath: {}", code).into());
+    }
+
+    Ok(std::str::from_utf8(&output.stdout)
+        .map_err(|e| format!("Error converting output to string: {}", e))?
+        .trim()
+        .to_string())
 }
